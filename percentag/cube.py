e5391dc9d0011a178dcd0fadcc4185c61bb5d017
HorseChair28 <nuff@inbox.ru>
2022-05-16 09:19:49 +0300

    version master

--- percentag/cube.py
+++ percentag/cube.py
@@ -1,9 +1,9 @@
 list_1=[]
-for i in range(1, 50, 2):
+for i in range(1, 1000, 2):
     i=i**3
     list_1.append(i)
-# print(list_1)
 list_2=[]
+list_3=[]
 for j in list_1:
     a=j%10
     b=j//10%10
@@ -15,24 +15,19 @@ for j in list_1:
     z=a+b+c+d+e+f+g
     if z%7==0:
         list_2.append(j)
+for j in list_1:
+    k=j+17
+    ax=k % 10
+    bx=k // 10 % 10
+    cx=k // 10 // 10 % 10
+    dx=k // 10 // 10 // 10 % 10
+    ex=k // 10 // 10 // 10 // 10 % 10
+    fx=k // 10 // 10 // 10 // 10 // 10 % 10
+    gx=k // 10 // 10 // 10 // 10 // 10 // 10 % 10
+    zx=ax + bx + cx + dx + ex + fx + gx
+    if zx % 7 == 0:
+        list_3.append(k)
 print("Элементов в списке:", len(list_1))
-print("Сумма элементов, чья сумма цифр делится на 7:", sum(list_2))
-list_3=[]
-for i in list_1:
-    x = i + 17
-    list_3.append(x)
-    for j in list_3:
-        h=j%10
-        v=j//10%10
-        k=j//10//10%10
-        l=j//10//10//10%10
-        m=j//10//10//10//10%10
-        n=j//10//10//10//10//10%10
-        o=j//10//10//10//10//10//10%10
-        y=h+v+k+l+m+n+o
-    if y%7==0:
-        list_3.append(y)
-print(list_3)
-
-
+print("Сумма элементов, чья сумма цифр делится на 7:", sum(list_2),",и таких элементов", len(list_2))
+print("Сумма элементов, чья сумма цифр делится на 7 при добавлении 17:",sum(list_3),",и таких элементов", len(list_3))
 
